b8413d97fbcd09eca8ba6482e19ac7e1c9fb0565
Guillaume Salicis <gsalicis@fastbooking.net>
2018-07-25 11:00:46 +0200

    perf: undo wrong refacto 😠

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict'
-// o(n²)
+
 function countIdenticalPairs(array) {
   const foundAll = {} // integer found in array
-  const havePair = []
+  const havePair = {}
   array.forEach((item) => {
-    if (foundAll[item] && !havePair.includes(item)) {
-      havePair.push(item) // detect number that have been found
+    if (foundAll[item] && !havePair[item]) {
+      havePair[item] = true // detect number that have been found
     }
     foundAll[item] = (foundAll[item] || 0) + 1
   })
-  return havePair.reduce((acc, key) =>
+  return Object.keys(havePair).reduce((acc, key) =>
     acc + sumN(foundAll[key]), 0)
 }
 
